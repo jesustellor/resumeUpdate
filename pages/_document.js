cb68e8b7f98f2e2b17ec5ab0c2c5bfed0b5d4f66
jesustellor <jesustellor@gmail.com>
2023-01-29 19:16:58 -0800

    proud of myself and how far i have come!

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine&display=optional"/>
+      <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto&display=optional' />
+      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+
+      </Head>
       <body>
         <Main />
         <NextScript />
